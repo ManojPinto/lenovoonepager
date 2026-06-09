e5da6cbde38bd585e13c6a1e94d55720419f6d70
ManojPinto <mpintoo@lenovo.com>
2026-06-09 18:46:41 +0530

    Update lenovo_one_pager.py

--- lenovo_one_pager.py
+++ lenovo_one_pager.py
@@ -151,6 +151,7 @@ ALLOWED_USERS = {
     'abharadwaj1@lenovo.com': 'Anurag Bharadwaj',
     'abhimawad@lenovo.com': 'ARYAN BHIMAWAD',
     'abhongade@lenovo.com': 'Amol Shioshankar Bhongade',
+    'sdutta11@lenovo.com': 'Suvadeep Dutta',
     'lrebello@lenovo.com': 'Lio',
     'abiswal3@lenovo.com': 'Ankit Biswal',
     'achaggan@lenovo.com': 'Abdul Razak Chaggan',
